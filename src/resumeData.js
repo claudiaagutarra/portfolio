68cb19d3047abed9046dba227b91ca366337d142
Claudia Gutarra <claudiaagutarra@gmail.com>
2020-07-31 09:58:38 -0400

    all changes

---
+++ src/resumeData.js
@@ -0,0 +1,103 @@
+let resumeData = {
+  "imagebaseurl": "https://i.ibb.co/sJnph9Y/download.png",
+  "name": "Claudia Gutarra",
+  "role": "Full Stack Web Developer",
+  "linkedinId": "claudiaagutarra",
+  "skypeid": "claudiaagutarra",
+  "roleDescription": " Creative and hard working Full Stack Web Developer adept at building responsive, interactive, user-friendly websites. Passionate about perfecting and adding to my skillset, bringing ideas to life, and working with dedicated teams to build efficient web applications.",
+  "socialLinks": [
+    {
+      "name": "linkedin",
+      "url": "https://www.linkedin.com/in/claudiaagutarra/",
+      "className": "fa fa-linkedin"
+    },
+    {
+      "name": "github",
+      "url": "https://github.com/claudiaagutarra",
+      "className": "fa fa-github"
+    },
+
+  ],
+  "aboutme": "I am currently a Web Development Intern at Holy name Hospital in Teaneck, NJ. I've recently received my certification in Full Stack Development back in March, and I've been furthering my coding skillset ever since.",
+  "address": "Bogota, NJ",
+  "website": "https://github.com/claudiaagutarra",
+  "education": [
+    {
+      "UniversityName": "Rutgers University",
+      "specialization": "Full Stack Web Development",
+      "MonthOfPassing": "March",
+      "YearOfPassing": "2020",
+      "Achievements": "Most Likely to win Front-End Award"
+    },
+    {
+      "UniversityName": "Rutgers University",
+      "specialization": "ed.M, Education",
+      "MonthOfPassing": "May",
+      "YearOfPassing": "2019",
+      "Achievements": "Dean's List for more than 5 semesters"
+    }
+  ],
+  "work": [
+    {
+      "CompanyName": "Holy Name Hospital",
+      "specialization": "Web Development Intern",
+      "MonthOfLeaving": "Current",
+      "YearOfLeaving": "",
+      "Achievements": "Maintained websites and applications, including content updates, debugging, feature enhancements, and regular upgrades to ensure system usability. - Developed front-end web applications using HTML/CSS and JavaScript. Collaborated with senior-level developers to develop projects for websites through various phases of project lifecycle. Consulted with clients to understand their desires and suggest the best ideas to meet their website needs. Validated code for compatibility with different browsers and screen sizes."
+    },
+  ],
+  "skillsDescription": "Your skills here",
+  "skills": [
+    {
+      "skillname": "HTML5"
+    },
+    {
+      "skillname": "CSS"
+    },
+    {
+      "skillname": "Reactjs"
+    },
+    {
+      "skillname": "Bootstrap"
+    },
+    {
+      "skillname": "Javacript"
+    },
+    {
+      "skillname": "ASP.NET"
+    },
+    {
+      "skillname": "Mongo DB"
+    }
+  ],
+  "portfolio": [
+    {
+      "name": "Hip Replacement Site",
+      "description": "Website",
+      "imgurl": "images/portfolio/hip.png"
+    },
+    {
+      "name": "Fit BMS",
+      "description": "Website",
+      "imgurl": "images/portfolio/fitbms.png"
+    },
+    {
+      "name": "My KeyChain",
+      "description": "Website",
+      "imgurl": "images/portfolio/keychain.png"
+    },
+    {
+      "name": "Venues and Bites",
+      "description": "Website",
+      "imgurl": "images/portfolio/VenuesandBites.png"
+    }
+  ],
+  "testimonials": [
+    {
+      "description": "Intelligent, dependable, and highly creative, Claudia was a pleasure to work with. Claudia's ability to quickly grasp the concept of what we wanted to work on and her willingness to shoulder the front-end development responsibility for the whole application made her an invaluable asset to our team. As a group mate who has worked with Claudia on multiple class projects and saw how diligently she worked, I would recommend her to companies looking for a front-end or a full-stack web developer.",
+      "name": "Amiel Nicdao, Web Development Classmate"
+    },
+  ]
+}
+
+export default resumeData
