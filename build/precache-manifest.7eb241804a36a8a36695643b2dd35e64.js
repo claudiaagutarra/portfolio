8f4bf3fb349ca75bf3b3b82311f46774d28e7b47
Claudia Gutarra <claudiaagutarra@gmail.com>
2020-08-05 01:15:24 -0400

    deploying

---
+++ build/precache-manifest.7eb241804a36a8a36695643b2dd35e64.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f115865a6f0e60d9fee4",
+    "url": "/portfolio/public/static/css/main.0a740e32.chunk.css"
+  },
+  {
+    "revision": "f115865a6f0e60d9fee4",
+    "url": "/portfolio/public/static/js/main.f115865a.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/portfolio/public/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "0e33d97f0e0876000e19",
+    "url": "/portfolio/public/static/js/runtime~main.0e33d97f.js"
+  },
+  {
+    "revision": "f3f0534816804a8b8bc3792df4ce4850",
+    "url": "/portfolio/public/index.html"
+  }
+];
